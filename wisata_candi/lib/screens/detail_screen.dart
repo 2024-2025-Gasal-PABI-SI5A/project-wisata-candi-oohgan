d2d2a515d421b93d7b78478dc70f959a1d8aa23b
Steven Chandra <stevenchandrafei@gmail.com>
2024-11-09 11:37:03 +0700

    update: detail_screen.dart

--- wisata_candi/lib/screens/detail_screen.dart
+++ wisata_candi/lib/screens/detail_screen.dart
@@ -3,7 +3,7 @@ import 'package:wisata_candi/models/candi.dart';
 
 class DetailScreen extends StatelessWidget {
   final Candi candi;
-  
+
   const DetailScreen({super.key, required this.candi});
 
   @override
@@ -13,16 +13,49 @@ class DetailScreen extends StatelessWidget {
         children: [
           Stack(
             children: [
+              // Detail HEADER
+
+              // Image Utama
               Padding(
                 padding: const EdgeInsets.symmetric(horizontal: 8.0),
                 child: ClipRRect(
                   borderRadius: BorderRadius.circular(20),
-                  child: Image.asset(candi.imageAsset,
-                      width: double.infinity, height: 300, fit: BoxFit.cover),
+                  child: Image.asset(
+                    candi.imageAsset,
+                    width: double.infinity, 
+                    height: 300, 
+                    fit: BoxFit.cover
+                    ),
                 ),
               ),
+              // Back Button
+              Padding(
+                padding: const EdgeInsets.all(16),
+                child: Container(
+                  decoration: BoxDecoration(
+                    color: Colors.deepPurple[100]?.withOpacity(0.8),
+                    shape: BoxShape.circle,
+                  ),
+                  child: IconButton(
+                    onPressed: (){},
+                    icon: const Icon(Icons.arrow_back),
+                    ),
+                ),
+                )
             ],
           ),
+          // Detail Info
+          Padding(
+            padding: EdgeInsets.symmetric(horizontal: 16),
+            child: Column(
+              children: [
+                // Info Atas
+                // Info Tengah 
+                // Info Bawah
+              ],
+            )
+            ),
+          // Info Atas
         ],
       ),
     );
